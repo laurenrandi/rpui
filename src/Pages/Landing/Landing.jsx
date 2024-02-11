729e14b4773d09a986c9f8747ca84ebc05a07efd
randi <randi@LAPTOP-V6I43OC1>
2024-02-11 16:00:48 -0500

    Landing

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+
 
 const Landing = () => {
   
   return(
     <>
-      landing
+      <Typography> <div class="Welcome">Welcome to <strong>  SPARQ Profile Builder </strong>  </div>
+      <div class = "square">
+        <div class = "float-container">
+          <div class = "float-childleft">
+            <strong>ABOUT SPB</strong>
+          </div>
+          <div class = "float-childright">
+           SPB provides colleagues who are part of a professional services firm with an effective way to store, update, and product professional profiles for Client consideration. Think of these as configurable resumes. With SPB you will be able to enter and maintain your professional skills, experiences, education and capabilities in an easy to use platform. You can quickly produce PDF versions of customization profiles that can be shared.
+          </div>
+        </div>
+    
+      </div>
+      </Typography>
     </>
   );
 };
