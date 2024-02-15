11d3e1dcde0563599e4f3bf79f3d85241e6fdbd3
Tom Rusch <trusch@augusta.edu>
2024-02-14 19:49:49 -0500

    fix missing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProfileEditor from "./Pages/ProfileEditor/ProfileEditor";
 import Landing from "./Pages/Landing/Landing";
 import ErrorPage from "./Pages/ErrorPage/ErrorPage";
 import LoginSuccess from "./Pages/LoginSuccess/LoginSuccess";
