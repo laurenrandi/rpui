4ea4aa5adc05136ef67bc6a43f542e8d6369d687
tekeroberts <tekeroberts@yahoo.com>
2024-02-20 02:04:01 -0500

    added map to aboutviewer

--- src/Components/AboutViewer/AboutViewer.stories.js
+++ src/Components/AboutViewer/AboutViewer.stories.js
@@ -1,17 +1,42 @@
 import AboutViewer from './AboutViewer';
 import sampleProfile from '../../Lib/sampleProfile.json';
+import emptyProfile from '../../Lib/emptyProfile.json';
+import { Box } from '@mui/material';
+import { useFormik } from 'formik';
+
+const Wrapper = ({ initialValues }) => {
+  const formik = useFormik({
+    initialValues: initialValues,
+    enableReinitialize: true
+  });
+
+  return (
+    <AboutViewer formik={formik} />
+  )
+}
 
 export default {
   title: 'ProfileComponents/About',
-  component: AboutViewer,
+  component: Wrapper,
   parameters: {
     layout: 'centered',
-  }
+  },
+  decorators: [
+    Story => (
+      <Box width={500}>
+        <Story />
+      </Box>
+    )
+  ]
 };
 
-export const Default = {};
+export const Primary = {
+  args: {
+    initialValues: emptyProfile
+  }
+};
 export const Populated = {
   args: {
-    about: sampleProfile.about
+    initialValues: sampleProfile
   }
 };
